796557f3f5df13e8b2ab37fb11f0556cdd12e84d
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-07-27 09:58:00 -0400

    limit max number of samples to test on

--- meandian/meandian_include_test.py
+++ meandian/meandian_include_test.py
@@ -1,21 +1,25 @@
 from meandian_include import meandian
 import numpy as np
-from hypothesis import given
+from hypothesis import given, settings
 from hypothesis import strategies as st
 
 
-@given(st.integers(1), st.floats(1, 2))
+@given(st.integers(1, 200), st.floats(1, 2))
 def test_median(n, alpha):
-    x = np.random.rand(n+1)
+    x = np.random.rand(n*2+1)
     # test the median
     np.testing.assert_allclose(np.median(x), meandian(x, 1))
     # test the mean
     np.testing.assert_allclose(np.mean(x), meandian(x, 2))
+    # test if it throws an error
+    meandian(x, alpha)
 
-def test_median_int():
-    x = [1, 3, 5, 8, 8]
+@given(st.integers(1, 200), st.floats(1, 2))
+def test_median_int(n, alpha):
+    x = np.random.randint(-10, 20, size=(n*2+1))
     # test the median
     np.testing.assert_allclose(np.median(x), meandian(x, 1))
     # test the mean
     np.testing.assert_allclose(np.mean(x), meandian(x, 2))
-    #assert np.mean(x) <= meandian(x, alpha) <= np.mean(x, 2)
+    # test if it throws an error
+    meandian(x, alpha)
